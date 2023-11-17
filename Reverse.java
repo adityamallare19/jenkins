9fc7fb4409de11554177fa7b2beee46ee1562b02
adityamallare19 <151202208+adityamallare19@users.noreply.github.com>
2023-11-17 22:11:29 +0530

    Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,28 @@
+public class Reverse {
+    public static void main(String[] args) {
+        String original = "Hello, World!";
+        String reversed = reverseString(original);
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+    }
+
+    public static String reverseString(String input) {
+        char[] charArray = input.toCharArray();
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+            // Swap characters at start and end indices
+            char temp = charArray[start];
+            charArray[start] = charArray[end];
+            charArray[end] = temp;
+
+            // Move towards the center
+            start++;
+            end--;
+        }
+
+        // Convert the char array back to a string
+        return new String(charArray);
+    }
+}
